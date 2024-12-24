23e39b9c424cab5fa63523fecf4eab4f4bb12f55
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-24 22:11:05 +0630

    Feat : Add EnumRespType

---
+++ HospitalBookingSystem.Domain/EnumRespType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HospitalBookingSystem.Domain
+{
+    public enum EnumRespType
+    {
+        None,
+        Success,
+        ValidationError,
+        SystemError
+    }
+}

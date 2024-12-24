aae4bae6e6b8a3f174d946646ea7663cc432bcc6
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-24 21:31:09 +0630

    Feat : Add DoctorModel

--- HospitalBookingSystem.Domain/Model/Doctor/DoctorModel.cs
+++ HospitalBookingSystem.Domain/Model/Doctor/DoctorModel.cs
@@ -6,7 +6,25 @@ using System.Threading.Tasks;
 
 namespace HospitalBookingSystem.Domain.Model.Doctor
 {
-    internal class DoctorModel
+    public class DoctorModel
     {
+        public string DoctorId { get; set; } = null!;
+
+        public string Name { get; set; } = null!;
+
+        public string Specialization { get; set; } = null!;
+
+        public string PhoneNumber { get; set; } = null!;
+
+        public string Email { get; set; } = null!;
+
+        public int Experience { get; set; }
+
+        public string Department { get; set; } = null!;
+
+        public string LicenseNumber { get; set; } = null!;
+
+        public bool IsAvailable { get; set; }
+
     }
 }

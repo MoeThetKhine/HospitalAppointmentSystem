a8111452bcaaca01e1da96e4d68aa8fd99780af4
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-24 21:32:56 +0630

    Feat : Add PatientModel

--- HospitalBookingSystem.Domain/Model/Patient/PatientModel.cs
+++ HospitalBookingSystem.Domain/Model/Patient/PatientModel.cs
@@ -6,7 +6,26 @@ using System.Threading.Tasks;
 
 namespace HospitalBookingSystem.Domain.Model.Patient
 {
-    internal class PatientModel
+    public class PatientModel
     {
+        public string PatientId { get; set; } = null!;
+
+        public string Name { get; set; } = null!;
+
+        public DateOnly DateOfBirth { get; set; }
+
+        public string Gender { get; set; } = null!;
+
+        public string PhoneNumber { get; set; } = null!;
+
+        public string Email { get; set; } = null!;
+
+        public string Address { get; set; } = null!;
+
+        public string MedicalHistory { get; set; } = null!;
+
+        public string EmergencyContact { get; set; } = null!;
+
+        public string InsuranceDetails { get; set; } = null!;
     }
 }

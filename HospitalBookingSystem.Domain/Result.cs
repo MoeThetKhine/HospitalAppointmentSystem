047365720305f4bdcb907f6c4287c9da69ab5c58
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-24 22:22:02 +0630

    -

--- HospitalBookingSystem.Domain/Result.cs
+++ HospitalBookingSystem.Domain/Result.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace HospitalBookingSystem.Domain
+﻿namespace HospitalBookingSystem.Domain
 {
     public class Result<T>
     {

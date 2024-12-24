4fbe50e08bf8aa39407ba6ce4f2f1a04d38e1ab1
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-24 21:33:18 +0630

    Format Code

--- HospitalBookingSystem.Domain/Model/Patient/PatientModel.cs
+++ HospitalBookingSystem.Domain/Model/Patient/PatientModel.cs
@@ -1,25 +1,24 @@
-﻿namespace HospitalBookingSystem.Domain.Model.Patient
+﻿namespace HospitalBookingSystem.Domain.Model.Patient;
+
+public class PatientModel
 {
-    public class PatientModel
-    {
-        public string PatientId { get; set; } = null!;
+    public string PatientId { get; set; } = null!;
 
-        public string Name { get; set; } = null!;
+    public string Name { get; set; } = null!;
 
-        public DateOnly DateOfBirth { get; set; }
+    public DateOnly DateOfBirth { get; set; }
 
-        public string Gender { get; set; } = null!;
+    public string Gender { get; set; } = null!;
 
-        public string PhoneNumber { get; set; } = null!;
+    public string PhoneNumber { get; set; } = null!;
 
-        public string Email { get; set; } = null!;
+    public string Email { get; set; } = null!;
 
-        public string Address { get; set; } = null!;
+    public string Address { get; set; } = null!;
 
-        public string MedicalHistory { get; set; } = null!;
+    public string MedicalHistory { get; set; } = null!;
 
-        public string EmergencyContact { get; set; } = null!;
+    public string EmergencyContact { get; set; } = null!;
 
-        public string InsuranceDetails { get; set; } = null!;
-    }
+    public string InsuranceDetails { get; set; } = null!;
 }

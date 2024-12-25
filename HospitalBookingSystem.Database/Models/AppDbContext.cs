d0ec734179bdadd1c1d2cd1a01b67baef56d8c5a
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-25 13:31:03 +0630

    -

--- HospitalBookingSystem.Database/Models/AppDbContext.cs
+++ HospitalBookingSystem.Database/Models/AppDbContext.cs
@@ -11,11 +11,15 @@ public partial class AppDbContext : DbContext
     {
     }
 
+    #region DbSet
+
     public virtual DbSet<TblAppointment> TblAppointments { get; set; }
 
     public virtual DbSet<TblDoctor> TblDoctors { get; set; }
 
     public virtual DbSet<TblPatient> TblPatients { get; set; }
+
+    #endregion
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.

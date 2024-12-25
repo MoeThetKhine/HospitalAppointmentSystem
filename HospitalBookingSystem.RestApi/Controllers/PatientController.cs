c43d5980f44cdf84c41d10622a18b4a59aba9be6
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-25 13:43:27 +0630

    Format Code

--- HospitalBookingSystem.RestApi/Controllers/PatientController.cs
+++ HospitalBookingSystem.RestApi/Controllers/PatientController.cs
@@ -1,50 +1,49 @@
-﻿namespace HospitalBookingSystem.RestApi.Controllers
+﻿namespace HospitalBookingSystem.RestApi.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class PatientController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class PatientController : ControllerBase
+    private readonly PatientService _service;
+
+    public PatientController(PatientService service)
     {
-        private readonly PatientService _service;
-
-        public PatientController(PatientService service)
-        {
-            _service = service;
-        }
-
-        #region GetPatientAsync
-
-        [HttpGet]
-        public async Task<IActionResult> GetPatientAsync()
-        {
-            try
-            {
-                var lst = await _service.GetPatientAsyn();
-                return Ok(lst);
-            }
-            catch (Exception ex)
-            {
-                return StatusCode(500, ex.Message);
-            }
-        }
-
-        #endregion
-
-        #region CreatePatientAsync
-
-        [HttpPost]
-        public async Task<IActionResult> CreatePatientAsync([FromForm]PatientRequestModel requestModel)
-        {
-            try
-            {
-                var item = await _service.CreatePatientAsync(requestModel);
-                return Ok(item);
-            }
-            catch(Exception ex)
-            {
-                return StatusCode(500, ex.Message);
-            }
-        }
-
-        #endregion
+        _service = service;
     }
+
+    #region GetPatientAsync
+
+    [HttpGet]
+    public async Task<IActionResult> GetPatientAsync()
+    {
+        try
+        {
+            var lst = await _service.GetPatientAsyn();
+            return Ok(lst);
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, ex.Message);
+        }
+    }
+
+    #endregion
+
+    #region CreatePatientAsync
+
+    [HttpPost]
+    public async Task<IActionResult> CreatePatientAsync([FromForm]PatientRequestModel requestModel)
+    {
+        try
+        {
+            var item = await _service.CreatePatientAsync(requestModel);
+            return Ok(item);
+        }
+        catch(Exception ex)
+        {
+            return StatusCode(500, ex.Message);
+        }
+    }
+
+    #endregion
 }

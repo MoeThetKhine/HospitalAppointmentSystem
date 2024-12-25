3109e5f0a5e7899f14fff82bcf7bbe209ca3df3f
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-25 13:36:27 +0630

    -

--- HospitalBookingSystem.Domain/Model/Patient/PatientRequestModel.cs
+++ HospitalBookingSystem.Domain/Model/Patient/PatientRequestModel.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace HospitalBookingSystem.Domain.Model.Patient
+﻿namespace HospitalBookingSystem.Domain.Model.Patient
 {
     public class PatientRequestModel
     {

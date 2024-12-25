25f2c6685ccb7a5b2cb08671d6e066f58f14f083
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-25 12:44:01 +0630

    Feat : Add PatientRequestModel

---
+++ HospitalBookingSystem.Domain/Model/Patient/PatientRequestModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HospitalBookingSystem.Domain.Model.Patient
+{
+    public class PatientRequestModel
+    {
+        public string Name { get; set; } = null!;
+
+        public DateOnly DateOfBirth { get; set; }
+
+        public string Gender { get; set; } = null!;
+
+        public string PhoneNumber { get; set; } = null!;
+
+        public string Email { get; set; } = null!;
+
+        public string Address { get; set; } = null!;
+
+        public string MedicalHistory { get; set; } = null!;
+
+        public string EmergencyContact { get; set; } = null!;
+
+        public string InsuranceDetails { get; set; } = null!;
+    }
+}

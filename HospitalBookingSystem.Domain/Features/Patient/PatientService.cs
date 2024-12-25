62902a60b89ca5f13ec34fc62072a5993e38ec87
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-25 13:38:15 +0630

    Format Code

--- HospitalBookingSystem.Domain/Features/Patient/PatientService.cs
+++ HospitalBookingSystem.Domain/Features/Patient/PatientService.cs
@@ -1,120 +1,119 @@
-﻿namespace HospitalBookingSystem.Domain.Features.Patient
+﻿namespace HospitalBookingSystem.Domain.Features.Patient;
+
+public class PatientService
 {
-    public class PatientService
+    private readonly AppDbContext _appDbContext;
+
+    public PatientService(AppDbContext appDbContext)
     {
-        private readonly AppDbContext _appDbContext;
+        _appDbContext = appDbContext;
+    }
 
-        public PatientService(AppDbContext appDbContext)
+    public async Task<Result<List<PatientModel>>> GetPatientAsyn()
+    {
+        Result<List<PatientModel>> response;
+
+        try
         {
-            _appDbContext = appDbContext;
-        }
+            var patient = _appDbContext.TblPatients.AsNoTracking();
 
-        public async Task<Result<List<PatientModel>>> GetPatientAsyn()
+            if (patient is null)
+            {
+                return Result<List<PatientModel>>.ValidationError("No Patient Found");
+            }
+
+            var lst = await patient.Select(x => new PatientModel()
+            {
+                PatientId = x.PatientId,
+                Name = x.Name,
+                DateOfBirth = x.DateOfBirth,
+                Gender = x.Gender,
+                PhoneNumber = x.PhoneNumber,
+                Email = x.Email,
+                Address = x.Address,
+                MedicalHistory = x.MedicalHistory,
+                EmergencyContact = x.EmergencyContact,
+                InsuranceDetails = x.InsuranceDetails
+            }).ToListAsync();
+
+            response =  Result<List<PatientModel>>.Success(lst); 
+        }
+        catch (Exception ex)
         {
-            Result<List<PatientModel>> response;
-
-            try
-            {
-                var patient = _appDbContext.TblPatients.AsNoTracking();
-
-                if (patient is null)
-                {
-                    return Result<List<PatientModel>>.ValidationError("No Patient Found");
-                }
-
-                var lst = await patient.Select(x => new PatientModel()
-                {
-                    PatientId = x.PatientId,
-                    Name = x.Name,
-                    DateOfBirth = x.DateOfBirth,
-                    Gender = x.Gender,
-                    PhoneNumber = x.PhoneNumber,
-                    Email = x.Email,
-                    Address = x.Address,
-                    MedicalHistory = x.MedicalHistory,
-                    EmergencyContact = x.EmergencyContact,
-                    InsuranceDetails = x.InsuranceDetails
-                }).ToListAsync();
-
-                response =  Result<List<PatientModel>>.Success(lst); 
-            }
-            catch (Exception ex)
-            {
-                response =  Result<List<PatientModel>>.SystemError(ex.Message);
-            }
-            return response;
+            response =  Result<List<PatientModel>>.SystemError(ex.Message);
         }
+        return response;
+    }
 
-        public async Task<Result<PatientRequestModel>> CreatePatientAsync(PatientRequestModel requestModel)
+    public async Task<Result<PatientRequestModel>> CreatePatientAsync(PatientRequestModel requestModel)
+    {
+        Result<PatientRequestModel> response;
+
+        try
         {
-            Result<PatientRequestModel> response;
-
-            try
+            if(requestModel is null)
             {
-                if(requestModel is null)
-                {
-                    response = Result<PatientRequestModel>.ValidationError("Please field all field");
-                }
-                if (requestModel.Name.IsNullOrEmpty())
-                {
-                    response = Result<PatientRequestModel>.ValidationError("Patient Name is required.");
-                }
-                if(requestModel.DateOfBirth == null)
-                {
-                    response = Result<PatientRequestModel>.ValidationError("Date Of Birth is required.");
-                }
-                if (requestModel.Gender.IsNullOrEmpty())
-                {
-                    response = Result<PatientRequestModel>.ValidationError("Gender is required.");
-                }
-                if (requestModel.PhoneNumber.IsNullOrEmpty())
-                {
-                    response = Result<PatientRequestModel>.ValidationError("Phone Number is required.");
-                }
-                if (requestModel.Email.IsNullOrEmpty())
-                {
-                    response = Result<PatientRequestModel>.ValidationError("Email is required");
-                }
-                if (requestModel.Address.IsNullOrEmpty())
-                {
-                    response = Result<PatientRequestModel>.ValidationError("Address is required");
-                }
-                if (requestModel.MedicalHistory.IsNullOrEmpty())
-                {
-                    response = Result<PatientRequestModel>.ValidationError("Medial History is required.");
-                }
-                if (requestModel.EmergencyContact.IsNullOrEmpty())
-                {
-                    response = Result<PatientRequestModel>.ValidationError("Emergeny Contat is required.");
-                }
-                if (requestModel.InsuranceDetails.IsNullOrEmpty())
-                {
-                    response = Result<PatientRequestModel>.ValidationError("Insurane Details is required.");
-                }
-
-                var patient = new TblPatient
-                {
-                    Name = requestModel.Name,
-                    DateOfBirth = requestModel.DateOfBirth,
-                    Gender = requestModel.Gender,
-                    PhoneNumber = requestModel.PhoneNumber,
-                    Email = requestModel.Email,
-                    Address = requestModel.Address,
-                    MedicalHistory = requestModel.MedicalHistory,
-                    EmergencyContact = requestModel.EmergencyContact,
-                    InsuranceDetails = requestModel.InsuranceDetails,
-                };
-
-                _appDbContext.TblPatients.AddAsync(patient);
-                await _appDbContext.SaveChangesAsync();
-
-                response = Result<PatientRequestModel>.Success(requestModel);
+                response = Result<PatientRequestModel>.ValidationError("Please field all field");
             }
-            catch(Exception ex)
+            if (requestModel.Name.IsNullOrEmpty())
             {
-                response = Result<PatientRequestModel>.SystemError(ex.Message);
+                response = Result<PatientRequestModel>.ValidationError("Patient Name is required.");
             }
-            return response;
+            if(requestModel.DateOfBirth == null)
+            {
+                response = Result<PatientRequestModel>.ValidationError("Date Of Birth is required.");
+            }
+            if (requestModel.Gender.IsNullOrEmpty())
+            {
+                response = Result<PatientRequestModel>.ValidationError("Gender is required.");
+            }
+            if (requestModel.PhoneNumber.IsNullOrEmpty())
+            {
+                response = Result<PatientRequestModel>.ValidationError("Phone Number is required.");
+            }
+            if (requestModel.Email.IsNullOrEmpty())
+            {
+                response = Result<PatientRequestModel>.ValidationError("Email is required");
+            }
+            if (requestModel.Address.IsNullOrEmpty())
+            {
+                response = Result<PatientRequestModel>.ValidationError("Address is required");
+            }
+            if (requestModel.MedicalHistory.IsNullOrEmpty())
+            {
+                response = Result<PatientRequestModel>.ValidationError("Medial History is required.");
+            }
+            if (requestModel.EmergencyContact.IsNullOrEmpty())
+            {
+                response = Result<PatientRequestModel>.ValidationError("Emergeny Contat is required.");
+            }
+            if (requestModel.InsuranceDetails.IsNullOrEmpty())
+            {
+                response = Result<PatientRequestModel>.ValidationError("Insurane Details is required.");
+            }
+
+            var patient = new TblPatient
+            {
+                Name = requestModel.Name,
+                DateOfBirth = requestModel.DateOfBirth,
+                Gender = requestModel.Gender,
+                PhoneNumber = requestModel.PhoneNumber,
+                Email = requestModel.Email,
+                Address = requestModel.Address,
+                MedicalHistory = requestModel.MedicalHistory,
+                EmergencyContact = requestModel.EmergencyContact,
+                InsuranceDetails = requestModel.InsuranceDetails,
+            };
+
+            _appDbContext.TblPatients.AddAsync(patient);
+            await _appDbContext.SaveChangesAsync();
+
+            response = Result<PatientRequestModel>.Success(requestModel);
         }
+        catch(Exception ex)
+        {
+            response = Result<PatientRequestModel>.SystemError(ex.Message);
+        }
+        return response;
     }
 }

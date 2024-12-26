eabe5a8f602a2a24f1e2b5d50294c8c346f857ae
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-26 17:20:25 +0630

    Feat : Add PatientResponseModel

---
+++ HospitalBookingSystem.Domain/Model/Patient/PatientResponseModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HospitalBookingSystem.Domain.Model.Patient
+{
+    public class PatientResponseModel
+    {
+        public string PhoneNumber { get; set; } = null!;
+        public string Address { get; set; } = null!;
+        public string MedicalHistory { get; set; } = null!;
+        public string EmergencyContact { get; set; } = null!;
+
+    }
+}

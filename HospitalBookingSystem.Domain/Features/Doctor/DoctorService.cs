162843404b14a8f2c71c1ef016628869691e4a2d
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-26 11:21:27 +0630

    -

--- HospitalBookingSystem.Domain/Features/Doctor/DoctorService.cs
+++ HospitalBookingSystem.Domain/Features/Doctor/DoctorService.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace HospitalBookingSystem.Domain.Features.Doctor
+﻿namespace HospitalBookingSystem.Domain.Features.Doctor
 {
-    internal class DoctorService
+    public class DoctorService
     {
     }
 }

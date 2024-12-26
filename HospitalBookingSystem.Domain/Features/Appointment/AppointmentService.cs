7e57e31feee5cacb32083ffeb0dc7cabf94d8e0b
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-26 11:21:41 +0630

    -

--- HospitalBookingSystem.Domain/Features/Appointment/AppointmentService.cs
+++ HospitalBookingSystem.Domain/Features/Appointment/AppointmentService.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace HospitalBookingSystem.Domain.Features.Appointment
+﻿namespace HospitalBookingSystem.Domain.Features.Appointment
 {
-    internal class AppointmentService
+    public class AppointmentService
     {
     }
 }

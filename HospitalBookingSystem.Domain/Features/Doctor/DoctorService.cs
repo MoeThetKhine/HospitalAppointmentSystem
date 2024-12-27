89d4f746eee1ba75beb10c34010919b029ffc338
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-27 13:12:09 +0630

    Format Code

--- HospitalBookingSystem.Domain/Features/Doctor/DoctorService.cs
+++ HospitalBookingSystem.Domain/Features/Doctor/DoctorService.cs
@@ -1,76 +1,75 @@
-﻿namespace HospitalBookingSystem.Domain.Features.Doctor
+﻿namespace HospitalBookingSystem.Domain.Features.Doctor;
+
+public class DoctorService
 {
-    public class DoctorService
+    private readonly AppDbContext _appDbContext;
+
+    public DoctorService(AppDbContext appDbContext)
     {
-        private readonly AppDbContext _appDbContext;
+        _appDbContext = appDbContext;
+    }
 
-        public DoctorService(AppDbContext appDbContext)
+    public async Task<Result<List<DoctorRequestModel>>> GetDoctorAsync()
+    {
+        Result<List<DoctorRequestModel>> result;
+
+        try
         {
-            _appDbContext = appDbContext;
-        }
+            var doctor = _appDbContext.TblDoctors.AsNoTracking();
 
-        public async Task<Result<List<DoctorRequestModel>>> GetDoctorAsync()
+            if(doctor is null)
+            {
+                result = Result<List<DoctorRequestModel>>.ValidationError("No Doctor Found");
+            }
+
+            var lst = await doctor.Select(x => new DoctorRequestModel()
+            {
+                Name = x.Name,
+                Specialization =x.Specialization,
+                PhoneNumber = x.PhoneNumber,
+                Email = x.Email,
+                Address = x.Address,
+            }).ToListAsync();
+
+            result = Result<List<DoctorRequestModel>>.Success(lst);
+        }
+        catch (Exception ex)
         {
-            Result<List<DoctorRequestModel>> result;
-
-            try
-            {
-                var doctor = _appDbContext.TblDoctors.AsNoTracking();
-
-                if(doctor is null)
-                {
-                    result = Result<List<DoctorRequestModel>>.ValidationError("No Doctor Found");
-                }
-
-                var lst = await doctor.Select(x => new DoctorRequestModel()
-                {
-                    Name = x.Name,
-                    Specialization =x.Specialization,
-                    PhoneNumber = x.PhoneNumber,
-                    Email = x.Email,
-                    Address = x.Address,
-                }).ToListAsync();
-
-                result = Result<List<DoctorRequestModel>>.Success(lst);
-            }
-            catch (Exception ex)
-            {
-                result = Result<List<DoctorRequestModel>>.SystemError(ex.Message);
-            }
-            return result;
+            result = Result<List<DoctorRequestModel>>.SystemError(ex.Message);
         }
+        return result;
+    }
 
-        public async Task<Result<List<DoctorRequestModel>>> GetDoctorByNameAsync(string name)
+    public async Task<Result<List<DoctorRequestModel>>> GetDoctorByNameAsync(string name)
+    {
+        Result<List<DoctorRequestModel>> result;
+
+        try
         {
-            Result<List<DoctorRequestModel>> result;
+            var doctor = _appDbContext.TblDoctors
+                .Where(x => x.Name == name)
+                .AsNoTracking();
 
-            try
+            if (doctor is null)
             {
-                var doctor = _appDbContext.TblDoctors
-                    .Where(x => x.Name == name)
-                    .AsNoTracking();
-
-                if (doctor is null)
-                {
-                    result = Result<List<DoctorRequestModel>>.ValidationError("No Doctor Found");
-                }
-               
-                var lst = await doctor.Select(x => new DoctorRequestModel()
-                {
-                    Name = x.Name,
-                    Specialization = x.Specialization,
-                    PhoneNumber = x.PhoneNumber,
-                    Email = x.Email,
-                    Address = x.Address,
-                }).ToListAsync();
-
-                result = Result<List<DoctorRequestModel>>.Success(lst);
+                result = Result<List<DoctorRequestModel>>.ValidationError("No Doctor Found");
             }
-            catch (Exception ex)
+           
+            var lst = await doctor.Select(x => new DoctorRequestModel()
             {
-                result = Result<List<DoctorRequestModel>>.SystemError(ex.Message);
-            }
-            return result;
+                Name = x.Name,
+                Specialization = x.Specialization,
+                PhoneNumber = x.PhoneNumber,
+                Email = x.Email,
+                Address = x.Address,
+            }).ToListAsync();
+
+            result = Result<List<DoctorRequestModel>>.Success(lst);
         }
+        catch (Exception ex)
+        {
+            result = Result<List<DoctorRequestModel>>.SystemError(ex.Message);
+        }
+        return result;
     }
 }

86100dd15d7cf9c7bb298cefcde7335b71caed61
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-28 00:44:38 +0630

    Format Code

--- HospitalBookingSystem.Domain/Features/Appointment/AppointmentService.cs
+++ HospitalBookingSystem.Domain/Features/Appointment/AppointmentService.cs
@@ -1,48 +1,47 @@
-﻿namespace HospitalBookingSystem.Domain.Features.Appointment
+﻿namespace HospitalBookingSystem.Domain.Features.Appointment;
+
+public class AppointmentService
 {
-    public class AppointmentService
+    private readonly AppDbContext _context;
+
+    public AppointmentService(AppDbContext context)
     {
-        private readonly AppDbContext _context;
-
-        public AppointmentService(AppDbContext context)
-        {
-            _context = context;
-        }
-
-        #region GetAppointmentListAsync
-
-        public async Task<Result<List<AppointmentRequestModel>>> GetAppointmentListAsync()
-        {
-            Result<List<AppointmentRequestModel>> result;
-
-            try
-            {
-                var appointment = _context.TblAppointments.AsNoTracking();
-
-                if(appointment is null)
-                {
-                    result = Result<List<AppointmentRequestModel>>.ValidationError("No Data Found");
-                }
-
-                var lst = await appointment.Select(x => new AppointmentRequestModel()
-                {
-                    PatientId = x.PatientId,
-                    DoctorId = x.DoctorId,
-                    AppointmentDate =x.AppointmentDate,
-                    Reason = x.Reason,
-                    Status = x.Status,
-                }).ToListAsync();
-
-                result = Result<List<AppointmentRequestModel>>.Success(lst);
-            }
-            catch (Exception ex)
-            {
-                result = Result<List<AppointmentRequestModel>>.SystemError(ex.Message);
-            }
-            return result;
-        }
-
-        #endregion
-
+        _context = context;
     }
+
+    #region GetAppointmentListAsync
+
+    public async Task<Result<List<AppointmentRequestModel>>> GetAppointmentListAsync()
+    {
+        Result<List<AppointmentRequestModel>> result;
+
+        try
+        {
+            var appointment = _context.TblAppointments.AsNoTracking();
+
+            if(appointment is null)
+            {
+                result = Result<List<AppointmentRequestModel>>.ValidationError("No Data Found");
+            }
+
+            var lst = await appointment.Select(x => new AppointmentRequestModel()
+            {
+                PatientId = x.PatientId,
+                DoctorId = x.DoctorId,
+                AppointmentDate =x.AppointmentDate,
+                Reason = x.Reason,
+                Status = x.Status,
+            }).ToListAsync();
+
+            result = Result<List<AppointmentRequestModel>>.Success(lst);
+        }
+        catch (Exception ex)
+        {
+            result = Result<List<AppointmentRequestModel>>.SystemError(ex.Message);
+        }
+        return result;
+    }
+
+    #endregion
+
 }

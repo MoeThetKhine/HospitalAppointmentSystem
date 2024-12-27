eb3282872772213cc889a1814fb2cc737bd359e4
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-27 12:19:22 +0630

    Feat : Add GetDoctorAsync in Dotor Controller

--- HospitalBookingSystem.RestApi/Controllers/DoctorController.cs
+++ HospitalBookingSystem.RestApi/Controllers/DoctorController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using HospitalBookingSystem.Domain.Features.Doctor;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace HospitalBookingSystem.RestApi.Controllers
@@ -7,5 +8,17 @@ namespace HospitalBookingSystem.RestApi.Controllers
     [ApiController]
     public class DoctorController : ControllerBase
     {
+        private readonly DoctorService _service;
+
+        public DoctorController(DoctorService service)
+        {
+            _service = service;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetDoctorAsync()
+        {
+            var lst = await _service.GetDoctorAsync();
+            return Ok(lst);
+        }
     }
 }

becec9a832812304e8396a948f774d71389c6366
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-27 20:11:51 +0630

    Feat : Add AppointmentRequestModel

---
+++ HospitalBookingSystem.Domain/Model/Appointment/AppointmentRequestModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HospitalBookingSystem.Domain.Model.Appointment
+{
+    public class AppointmentRequestModel
+    {
+        public DateOnly Date { get; set; }
+
+        public TimeOnly Time { get; set; }
+
+        public string DoctorId { get; set; } = null!;
+
+        public string PatientId { get; set; } = null!;
+
+        public string Status { get; set; } = null!;
+
+        public string Reason { get; set; } = null!;
+
+        public string? RoomNumber { get; set; }
+
+        public DateOnly BookingDate { get; set; }
+
+        public string Remarks { get; set; } = null!;
+
+    }
+}

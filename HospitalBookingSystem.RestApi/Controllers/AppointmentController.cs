80b66bf0e19a5d660b50f23bc3cb13b2c4f6055b
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-28 00:43:26 +0630

    -

--- HospitalBookingSystem.RestApi/Controllers/AppointmentController.cs
+++ HospitalBookingSystem.RestApi/Controllers/AppointmentController.cs
@@ -1,7 +1,4 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-
-namespace HospitalBookingSystem.RestApi.Controllers
+﻿namespace HospitalBookingSystem.RestApi.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]

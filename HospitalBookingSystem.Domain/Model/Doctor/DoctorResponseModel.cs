f81a5ea96bb3c89af82e10925fb818ceec3efa89
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-27 16:49:01 +0630

    Format Code

--- HospitalBookingSystem.Domain/Model/Doctor/DoctorResponseModel.cs
+++ HospitalBookingSystem.Domain/Model/Doctor/DoctorResponseModel.cs
@@ -1,12 +1,11 @@
-﻿namespace HospitalBookingSystem.Domain.Model.Doctor
+﻿namespace HospitalBookingSystem.Domain.Model.Doctor;
+
+public class DoctorResponseModel
 {
-    public class DoctorResponseModel
-    {
-        public string Specialization { get; set; } = null!;
+    public string Specialization { get; set; } = null!;
 
-        public string PhoneNumber { get; set; } = null!;
+    public string PhoneNumber { get; set; } = null!;
 
-        public string Address { get; set; } = null!;
+    public string Address { get; set; } = null!;
 
-    }
 }

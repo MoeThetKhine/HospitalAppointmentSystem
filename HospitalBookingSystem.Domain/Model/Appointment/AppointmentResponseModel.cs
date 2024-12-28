c950773a7e49540eb1c8d148dc18604c0679dc75
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-28 09:53:32 +0630

    Feat : Add AppointmentResponseModel

---
+++ HospitalBookingSystem.Domain/Model/Appointment/AppointmentResponseModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HospitalBookingSystem.Domain.Model.Appointment
+{
+    public class AppointmentResponseModel
+    {
+        public string Status { get; set; } = null!;
+    }
+}
